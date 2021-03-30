aaee962ac3bddf5190c9b0088f8baa04126d6711
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-30 19:55:52 -0400

    Set up functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// Computer plays
+function computerPlay() {
+
+}
+
+// Player's selection
+function playerPlay() {
+
+}
+
+// One round of game
+function playRound(playerSelection, computerSelection) {
+
+}
+
+// Play game
+function game() {
+  
+}
