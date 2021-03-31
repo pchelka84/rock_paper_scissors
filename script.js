b7add85b4586aecdda8b90715f0a5cf12907898a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-30 20:14:37 -0400

    Handle users choice

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function computerPlay() {
 
 // Player's selection
 function playerPlay() {
-
+  let personSelection = prompt("Please make your selection.").toLowerCase();
+  if (personSelection !== 'rock' || personSelection !== 'paper' || 
+  personSelection !== 'scissors') {
+    personSelection = prompt("Please choose one of three - rock, paper, or scissors")
+  }
+  return personSelection;
 }
 
 // One round of game
